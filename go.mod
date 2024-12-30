31e4ec441b5216c42a7f35426a7e4e7e8b1cdaa6
Vladislav Negmatulin <vladislav.negmatulin@mego.travel>
2024-12-31 00:57:09 +0300

    add chi module and refactor

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-chi/chi/v5 v5.2.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
--- internal/app/server.go
+++ internal/app/server.go
@@ -2,8 +2,11 @@ package app
 
 import (
 	//"fmt"
+
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+"github.com/go-chi/chi/v5/middleware"
 	"github.com/VladSnap/shortener/internal/data"
 	"github.com/VladSnap/shortener/internal/handlers"
 )
@@ -13,14 +16,17 @@ func RunServer() error {
 	return http.ListenAndServe(`:8080`, httpListener)
 }
 
-func initServer() *http.ServeMux {
+func initServer() *chi.Mux {
 	shortLinkRepo := data.NewShortLinkRepo()
 	getHandler := handlers.NewGetHandler(shortLinkRepo)
 	postHandler := handlers.NewPostHandler(shortLinkRepo)
 
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, postHandler.Handle)
-	mux.HandleFunc(`/{id}`, getHandler.Handle)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+    r.Use(middleware.Recoverer)
 
-	return mux
+	r.Post("/", postHandler.Handle)
+	r.Get("/{id}", getHandler.Handle)
+
+	return r
 }
