a2493cb1413f499cd7fccd85bb9c833573a609d7
Vladislav Negmatulin <vladislav.negmatulin@mego.travel>
2025-01-21 23:12:24 +0300

    rename helpers.go file

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -9,12 +9,12 @@ var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func RandStringRunes(n int) (string, error) {
 	b := make([]rune, n)
-    maxLetters := big.NewInt(int64(len(letters)))
+	maxLetters := big.NewInt(int64(len(letters)))
 	for i := range b {
-        rndIndex, err := crypto.Int(crypto.Reader, maxLetters)
-        if err != nil {
-            return "", err
-        }
+		rndIndex, err := crypto.Int(crypto.Reader, maxLetters)
+		if err != nil {
+			return "", err
+		}
 		b[i] = letters[rndIndex.Int64()]
 	}
 	return string(b), nil
