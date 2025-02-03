24b1db9369ea8b4564beab4027e7e3dc4d19b6f9
Vladislav Negmatulin <negmatulin@mego.travel>
2025-02-03 04:37:35 +0300

    check path dir and refactor create and load file

--- internal/data/repos/FileShortLinkRepo.go
+++ internal/data/repos/FileShortLinkRepo.go
@@ -3,11 +3,13 @@ package repos
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 
 	"github.com/VladSnap/shortener/internal/constants"
 	"github.com/VladSnap/shortener/internal/data/models"
+	"github.com/VladSnap/shortener/internal/helpers"
 	"github.com/VladSnap/shortener/internal/log"
 	"github.com/google/uuid"
 )
@@ -19,20 +21,17 @@ type FileShortLinkRepo struct {
 
 func NewFileShortLinkRepo(fileStoragePath string) (*FileShortLinkRepo, error) {
 	repo := new(FileShortLinkRepo)
-	file, err := os.OpenFile(fileStoragePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, constants.FileRWPerm)
+	file, err := createFileStorage(fileStoragePath)
 	if err != nil {
-		return nil, fmt.Errorf("failed open file storage: %w", err)
+		return nil, fmt.Errorf("failed create file storage: %w", err)
 	}
 	repo.storageFile = file
-	links, err := repo.loadFromFile()
-	if err != nil {
-		return nil, fmt.Errorf("failed load from file storage: %w", err)
-	}
 
-	repo.links = make(map[string]models.ShortLinkData, len(links))
-	for _, link := range links {
-		repo.links[link.ShortURL] = link
+	links, err := repo.loadLinks()
+	if err != nil {
+		return nil, fmt.Errorf("failed load links: %w", err)
 	}
+	repo.links = links
 
 	return repo, nil
 }
@@ -99,4 +98,32 @@ func (repo *FileShortLinkRepo) writeLink(link models.ShortLinkData) error {
 		return fmt.Errorf("failed write to file storage: %w", err)
 	}
 	return nil
+}
+
+func createFileStorage(fileStoragePath string) (*os.File, error) {
+	ok, err := helpers.DirectoryExists(fileStoragePath)
+	if !ok && err == nil {
+		return nil, errors.New("fileStoragePath directory not exists")
+	} else if err != nil {
+		return nil, fmt.Errorf("failed check fileStoragePath: %w", err)
+	}
+
+	file, err := os.OpenFile(fileStoragePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, constants.FileRWPerm)
+	if err != nil {
+		return nil, fmt.Errorf("failed open file storage: %w", err)
+	}
+
+	return file, nil
+}
+
+func (repo *FileShortLinkRepo) loadLinks() (map[string]models.ShortLinkData, error) {
+	links, err := repo.loadFromFile()
+	if err != nil {
+		return nil, fmt.Errorf("failed load from file storage: %w", err)
+	}
+	linkMap := make(map[string]models.ShortLinkData, len(links))
+	for _, link := range links {
+		linkMap[link.ShortURL] = link
+	}
+	return linkMap, nil
 }
--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -4,6 +4,7 @@ import (
 	crypto "crypto/rand"
 	"fmt"
 	"math/big"
+	"os"
 )
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
@@ -19,4 +20,19 @@ func RandStringRunes(n int) (string, error) {
 		b[i] = letters[rndIndex.Int64()]
 	}
 	return string(b), nil
+}
+
+func DirectoryExists(path string) (bool, error) {
+	// Пытаемся получить информацию о пути.
+	_, err := os.Stat(path)
+	if err == nil {
+		// Если ошибки нет, проверяем, что это именно директория.
+		fileInfo, _ := os.Stat(path)
+		return fileInfo.IsDir(), nil
+	} else if os.IsNotExist(err) {
+		// Если ошибка указывает на отсутствие пути.
+		return false, nil
+	}
+	// Возвращаем любую другую ошибку.
+	return false, fmt.Errorf("failed check directory exists: %w", err)
 }
