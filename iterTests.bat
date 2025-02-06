befe15684779e287f5a8c49f476b4d1b5f250b67
Vladislav Negmatulin <negmatulin@mego.travel>
2025-02-07 02:37:29 +0300

    fix find migrations path for github ci/cd and apply migrations to db

--- iterTests.bat
+++ iterTests.bat
@@ -1,4 +1,4 @@
-go build -o shortener.exe main.go
+go build -o cmd\shortener\shortener.exe cmd\shortener\main.go
 
 shortenertest -test.v -test.run=^TestIteration1$ -binary-path=C:\Develop\go\projects\shortener\cmd\shortener\shortener.exe -source-path=C:\Develop\go\projects\shortener
 shortenertest -test.v -test.run=^TestIteration2$ -binary-path=C:\Develop\go\projects\shortener\cmd\shortener\shortener.exe -source-path=C:\Develop\go\projects\shortener
@@ -12,6 +12,6 @@ shortenertest -test.v -test.run=^TestIteration9$ -binary-path=C:\Develop\go\proj
 shortenertest -test.v -test.run=^TestIteration10$ -binary-path=C:\Develop\go\projects\shortener\cmd\shortener\shortener.exe -source-path=C:\Develop\go\projects\shortener -database-dsn='host=localhost user=postgres password=qwerty dbname=shortener sslmode=disable'
 shortenertest -test.v -test.run=^TestIteration11$ -binary-path=C:\Develop\go\projects\shortener\cmd\shortener\shortener.exe -source-path=C:\Develop\go\projects\shortener -database-dsn='host=localhost user=postgres password=qwerty dbname=shortener sslmode=disable'
 
-del shortener.exe
+del cmd\shortener\shortener.exe
 
 pause
