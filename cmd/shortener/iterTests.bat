df9c5e313bd4f2ad3597ac3e69d21444b0b59488
Vladislav Negmatulin <negmatulin@mego.travel>
2025-02-07 02:06:06 +0300

    add run shortenertest iter11

--- cmd/shortener/iterTests.bat
+++ cmd/shortener/iterTests.bat
@@ -10,6 +10,7 @@ shortenertest -test.v -test.run=^TestIteration7$ -binary-path=C:\Develop\go\proj
 shortenertest -test.v -test.run=^TestIteration8$ -binary-path=C:\Develop\go\projects\shortener\cmd\shortener\shortener.exe -source-path=C:\Develop\go\projects\shortener
 shortenertest -test.v -test.run=^TestIteration9$ -binary-path=C:\Develop\go\projects\shortener\cmd\shortener\shortener.exe -source-path=C:\Develop\go\projects\shortener -file-storage-path=D://shortenDataBase.json
 shortenertest -test.v -test.run=^TestIteration10$ -binary-path=C:\Develop\go\projects\shortener\cmd\shortener\shortener.exe -source-path=C:\Develop\go\projects\shortener -database-dsn='host=localhost user=postgres password=qwerty dbname=shortener sslmode=disable'
+shortenertest -test.v -test.run=^TestIteration11$ -binary-path=C:\Develop\go\projects\shortener\cmd\shortener\shortener.exe -source-path=C:\Develop\go\projects\shortener -database-dsn='host=localhost user=postgres password=qwerty dbname=shortener sslmode=disable'
 
 del shortener.exe
 
